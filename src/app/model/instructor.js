c688270b0d7b8b4540f448c817981b2148b65e3c
Luan Andrade <luan10017@hotmail.com>
2020-09-01 16:20:05 -0300

    Adicionado método delete

--- src/app/model/instructor.js
+++ src/app/model/instructor.js
@@ -4,7 +4,7 @@ const { date } = require('../../lib/utils')
 module.exports = {
     all(callback) {
         db.query(`SELECT * FROM instructors`, function(err, results){
-            if(err) return res.send("Database Error!")
+            if(err) throw `Database Error! ${err}`
 
             callback(results.rows)
         })
@@ -31,7 +31,7 @@ module.exports = {
     ]
 
     db.query(query, values, function(err, results){
-        if(err) return res.send("Database Error!")
+        if(err) throw `Database Error! ${err}`
         
         callback(results.rows[0])
     })
@@ -41,7 +41,7 @@ module.exports = {
             SELECT * 
             FROM instructors 
             WHERE id = $1`, [id], function(err, results){
-                if(err) return res.send("Database Error!")
+                if(err) throw `Database Error! ${err}`
                 callback(results.rows[0])
         })
     },
@@ -65,9 +65,16 @@ module.exports = {
         ]
 
         db.query(query, values, function(err, results) {
-            if(err) return res.send("Database Error!")
+            if(err) throw `Database Error! ${err}`
 
             callback()
+        })
+    },
+    delete(id, callback) {
+        db.query(`DELETE FROM instructors WHERE id = $1`, [id], function(err, results) {
+            if(err) throw `Database Error! ${err}`
+
+            return callback()
         })
     }
 }
