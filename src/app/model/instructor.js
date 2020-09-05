10d08f2234d0db61991aeb8030014d19a5c1f835
Luan Andrade <luan10017@hotmail.com>
2020-09-05 16:41:52 -0300

    Adicionada ideia de filtragem de instrutores

--- src/app/model/instructor.js
+++ src/app/model/instructor.js
@@ -3,9 +3,12 @@ const { date } = require('../../lib/utils')
 
 module.exports = {
     all(callback) {
-        db.query(`SELECT * 
-        FROM instructors 
-        ORDER BY name ASC`, function(err, results){
+        db.query(`
+        SELECT instructors.*, count(members) AS total_students
+        FROM instructors
+        LEFT JOIN members ON (members.instructor_id = instructors.id)
+        GROUP BY instructors.id
+        ORDER BY total_students DESC`, function(err, results){
             if(err) throw `Database Error! ${err}`
 
             callback(results.rows)
@@ -45,6 +48,20 @@ module.exports = {
             WHERE id = $1`, [id], function(err, results){
                 if(err) throw `Database Error! ${err}`
                 callback(results.rows[0])
+        })
+    },
+    findBy(filter, callback) {
+        db.query(`
+        SELECT instructors.*, count(members) AS total_students
+        FROM instructors
+        LEFT JOIN members ON (members.instructor_id = instructors.id)
+        WHERE instructors.name ILIKE '%${filter}%'
+        OR instructors.services ILIKE '%${filter}%'
+        GROUP BY instructors.id
+        ORDER BY total_students DESC`, function(err, results){
+            if(err) throw `Database Error! ${err}`
+
+            callback(results.rows)
         })
     },
     update(data, callback) {
