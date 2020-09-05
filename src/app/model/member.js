f52782e0fa7384e7688bd333fd1c2e331dd4c89b
Luan Andrade <luan10017@hotmail.com>
2020-09-05 16:40:54 -0300

    Criado relacionamento entre tabelas instructorns e members

--- src/app/model/member.js
+++ src/app/model/member.js
@@ -19,32 +19,35 @@ module.exports = {
             birth,
             blood,
             weight,
-            height
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            height,
+            instructor_id
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
         RETURNING id
-    `
-    const values = [
-        data.name,
-        data.avatar_url,
-        data.gender,
-        data.email,
-        date(data.birth).iso,
-        data.blood,
-        data.weight,
-        data.height
-    ]
+        `
+        const values = [
+            data.name,
+            data.avatar_url,
+            data.gender,
+            data.email,
+            date(data.birth).iso,
+            data.blood,
+            data.weight,
+            data.height,
+            data.instructor
+        ]
 
-    db.query(query, values, function(err, results){
-        if(err) throw `Database Error! ${err}`
-        
-        callback(results.rows[0])
-    })
+        db.query(query, values, function(err, results){
+            if(err) throw `Database Error! ${err}`
+            
+            callback(results.rows[0])
+        })
     },
     find(id, callback) {
         db.query (`
-            SELECT * 
+            SELECT members.*, instructors.name AS instructor_name
             FROM members 
-            WHERE id = $1`, [id], function(err, results){
+            LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+            WHERE members.id = $1`, [id], function(err, results){
                 if(err) throw `Database Error! ${err}`
                 callback(results.rows[0])
         })
@@ -59,8 +62,9 @@ module.exports = {
             email=($5),
             blood=($6),
             weight=($7),
-            height=($8)
-        WHERE id = $9
+            height=($8),
+            instructor_id=($9)
+        WHERE id = $10
         `
         const values = [
             data.avatar_url,
@@ -71,6 +75,7 @@ module.exports = {
             data.blood,
             data.weight,
             data.height,
+            data.instructor,
             data.id
         ]
 
@@ -85,6 +90,13 @@ module.exports = {
             if(err) throw `Database Error! ${err}`
 
             return callback()
+        })
+    },
+    instructorsSelectOptions(callback) {
+        db.query(`SELECT name, id FROM instructors`, function(err, results) {
+            if(err) throw `Database Error! ${err}`
+
+            callback(results.rows)
         })
     }
 }
