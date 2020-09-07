4f8a0dcf91f0491e412ed76fae5056ae0c2949bf
Luan Andrade <luan10017@hotmail.com>
2020-09-06 21:31:01 -0300

    Finalizada ideia de paginação e filtragem de dados

--- public/scripts.js
+++ public/scripts.js
@@ -9,7 +9,7 @@ for (item of menuItens) {
 
 /* PAGINAÇÃO */
 
-function pagination(selectedPage, totalPages) {
+function paginate(selectedPage, totalPages) {
     let pages = [],
         oldPage
 
@@ -35,4 +35,34 @@ function pagination(selectedPage, totalPages) {
     }
 
     return pages
+}
+
+function createPagination(pagination) {
+    
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
+
+    let elements = ""
+
+    for (let page of pages) {
+        if(String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            if(filter) {
+                    elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            }else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
+    }
+
+    pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+    createPagination(pagination)
 }
--- src/app/model/instructor.js
+++ src/app/model/instructor.js
@@ -98,9 +98,9 @@ module.exports = {
     },
     paginate(params) {
         const { filter, limit, offset, callback } = params
-
-        let query = "",
-            filterQuery = "",
+        
+        let query = ``,
+            filterQuery = ``,
             totalQuery = `(
                 SELECT count(*) FROM instructors
             ) AS total`
@@ -108,23 +108,25 @@ module.exports = {
         if (filter) {
             filterQuery = `
             WHERE instructors.name ILIKE '%${filter}%'
-            OR instructors.services ILIKE '%${filter}%
+            OR instructors.services ILIKE '%${filter}%'
             `
             totalQuery = `(
                 SELECT count(*) FROM instructors
                 ${filterQuery}
             ) AS total`
         }
-
+        
         query = `
-        SELECT instructors.*, ${totalQuery},count(members) AS total_students
+        SELECT instructors.*, ${totalQuery}, count(members) AS total_students
         FROM instructors
-        LEFT JOIN members ON (instructors.id = members.instructor_id)
+        LEFT JOIN members ON (members.instructor_id = instructors.id)
         ${filterQuery}
         GROUP BY instructors.id LIMIT $1 OFFSET $2
         `
+
+
         db.query(query, [limit, offset], function(err, results) {
-            if (err) throw 'Database Error!'
+            if (err) throw `Database Error! ${err}`
 
             callback(results.rows)
         })
